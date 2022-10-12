bffbdf721a0b61a7f4d59ed89f9d7591437121d3
lucas leon <lucas-pinamar@hotmail.com>
2022-10-11 23:05:13 -0300

    actualizando

--- js/main.js
+++ js/main.js
@@ -1,30 +1,91 @@
-let usd = 285
-let eur = 280 
-let rea = 57
 
-
-function salir(salir) {
-    alert ("Fin del proceso")
-    
+function salir() {
+    alert("Fin del proceso")
 }
 
-function validarOperacion(plata,cantidad,valorUnitario) {
+function validarOperacion( moneda, plata) {
 
-    if(plata>=cantidad*valorUnitario){
+    cantidad = Number (prompt("Ingrese la cantidad a comprar de " + moneda.nombre + " : "))
+    
 
-        plata-=cantidad*valorUnitario
-        alert("tu saldo restante es " + plata )
+    if (moneda.precio*cantidad<= plata) {
+
+        plata -= cantidad * moneda.precio
+        alert("tu saldo restante es " + plata)
         return plata
-    
+    } else {
+        alert("Fondos insuficientes")
     }
-    else{
-        alert("Monto superado")
-
-    }
-    return plata
-    
 }
 
+
+
+function Continuar(){
+opcion = prompt("Ingresar opcion: 1: Continuar , 2: Salir")
+
+while(opcion != 2) {
+
+    if(opcion === 1){
+        seleccionarTipoMoneda()
+    }
+    else {alert("opcion invalida")
+
+    opcion = prompt("Ingresar opcion: 1: Continuar , 2: Salir")
+
+    }   
+}
+salir()
+}
+
+function Moneda(nombre, precio) {
+    this.nombre = nombre;
+    this.precio= precio;
+ }
+
+console.log("Selecciona Tu Moneda")
+
+let dolares = new Moneda("DOLARES", 285)
+console.log(dolares.nombre)
+
+let euros = new Moneda("EUROS", 280)
+console.log(euros.nombre)
+
+let reales = new Moneda("REALES", 56)
+console.log(reales.nombre)
+
+
+let listaMonedas = [dolares, euros, reales]
+
+
+
+function seleccionarTipoMoneda(){
+
+
+    let seleccion = false
+    while(seleccion != true){
+
+        moneda = prompt("Ingresar Moneda: Dolares-Euros-Reales")
+
+        moneda = moneda.toUpperCase()
+
+        for(let i = 0; i<listaMonedas.length; i+=1){
+            if(listaMonedas[i].nombre ===  moneda){
+                seleccion = true
+                validarOperacion(listaMonedas[i],plata)
+                Continuar()
+                return listaMonedas[i]
+            }
+
+           
+    }
+    console.log("Seleccione un Personaje Valido")
+    }
+
+
+}
+
+
+/*
 function reales(plata) {
 
     alert("valor del real en pesos es " + rea)
@@ -47,10 +108,8 @@ function dolares(plata) {
 
     cantidad = Number (prompt("Ingrese la cantidad a comprar:"))
     validarOperacion(plata,cantidad,usd)
-    
-}
 
-function seleccionarTipoMoneda(moneda) {
+function seleccionarTipoMoneda() {
 
     plata = Number(prompt("Ingrese su saldo a utilizar"))
 
@@ -67,13 +126,13 @@ function seleccionarTipoMoneda(moneda) {
                 dolares(plata)
 
                 opcion = prompt("Ingresar opcion: 1:Dolares , 2:Euros , 3:Reales, 4:Salir")
-                
+
                 break;
 
             case "2":
-                 euros(plata)
+                euros(plata)
 
-                 opcion = prompt("Ingresar opcion: 1:Dolares , 2:Euros , 3:Reales, 4:Salir")
+                opcion = prompt("Ingresar opcion: 1:Dolares , 2:Euros , 3:Reales, 4:Salir")
 
                 break;
 
@@ -85,44 +144,48 @@ function seleccionarTipoMoneda(moneda) {
                 break;
 
             case "4":
-                salir(salir)
+                salir()
 
                 break;
 
             default:
                 console.log("Operacion invalida")
         }
-    
     }
 }
+*/
+
 function validarClave(clave) {
-    if(clave=== "1111"){
+    if (clave === "1111") {
+        plata = Number(prompt("Ingrese su saldo a utilizar"))
         seleccionarTipoMoneda()
-    }else{
+    } else {
         alert("Clave incorrecta")
-        
+
     }
 }
+
 function ingreseClave(nombre) {
-    if(nombre){
+    if (nombre) {
         let clave = prompt("Ingresar clave")
         validarClave(clave)
     }
 }
 
 function ingreseNombre(dni) {
-    if(dni){
+    if (dni) {
         let nombre = prompt("Ingresar nombre")
         ingreseClave(nombre)
     }
 }
 
 function ingreseDni(inicio) {
-    if(inicio){
-        let dni = prompt("Ingresar DNI")
+    if (inicio) {
+        let dni = Number(prompt("Ingresar DNI"))
         ingreseNombre(dni)
-  }
+    }
 }
+
 function Inicio() {
 
     let inicio = confirm("Ustedes esta ingresando a Compra de divisas")
@@ -130,7 +193,4 @@ function Inicio() {
 
 }
 
-
 Inicio()
-
-
